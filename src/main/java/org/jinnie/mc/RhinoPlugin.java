f477fb8161c9efd9a4fad74b7f759a30379dc7c2
Ivan Donchev <idonchev@gmail.com>
2014-04-28 21:47:38 +0300

    initial commit

---
+++ src/main/java/org/jinnie/mc/RhinoPlugin.java
@@ -0,0 +1,72 @@
+package org.jinnie.mc;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import javax.script.*;
+import java.io.*;
+
+/**
+ * Created by jinnie on 4/27/14.
+ */
+public class RhinoPlugin extends JavaPlugin implements Listener {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("eval")) {
+            try {
+                StringBuilder builder = new StringBuilder();
+
+                for (String s : args) {
+                    builder.append(" ");
+                    builder.append(s);
+                }
+
+                String jsCmd = builder.toString();
+
+                ScriptEngine engine =
+                        new ScriptEngineManager().getEngineByName("javascript");
+                getLogger().info("Trying to run: " + jsCmd);
+                Object result = engine.eval(jsCmd + ";'Command complete';", getCommonBindings(sender));
+                sender.sendMessage((String) result);
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("load")) {
+            try {
+                File file = new File("program.js");
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                Reader reader = new FileReader("program.js");
+
+                ScriptEngine engine =
+                        new ScriptEngineManager().getEngineByName("javascript");
+                Bindings bindings = getCommonBindings(sender);
+                getLogger().info("Trying to run js from file");
+                Object result = engine.eval(reader, bindings);
+                sender.sendMessage((String) result);
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    public Bindings getCommonBindings(CommandSender sender) {
+        Bindings bindings = new SimpleBindings();
+        bindings.put("player", sender);
+        bindings.put("world", sender instanceof Player ? ((Player) sender).getWorld():null);
+        bindings.put("location", sender instanceof Player ? ((Player) sender).getLocation():null);
+        bindings.put("DIRT", Material.DIRT);
+        return bindings;
+    }
+}
